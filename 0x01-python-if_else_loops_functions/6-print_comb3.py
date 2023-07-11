cf5fdaaacb9a921420da371006831bea322be7ed
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-11 16:41:30 -0700

    correction to task 6 to include format, task 7 to fix pycode style errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for digit_one in range(0, 8):
     for digit_two in range(1, 10):
         if digit_one < digit_two:
             print(f"{digit_one}{digit_two}", end = ", ")
-print(f"{digit_one + 1}{digit_two}")
+print("{}{}" .format(digit_one + 1, digit_two))
