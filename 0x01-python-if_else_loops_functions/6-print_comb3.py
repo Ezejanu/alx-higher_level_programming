85f8a2d3f9e0b134d5aa423bd9cb8b0ab7131de0
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-11 15:26:25 -0700

    task 6 - a program that prints all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for digit_one in range(0, 8):
+    for digit_two in range(1, 10):
+        if digit_one < digit_two:
+            print(f"{digit_one}{digit_two}", end = ", ")
+print(f"{digit_one + 1}{digit_two}")
