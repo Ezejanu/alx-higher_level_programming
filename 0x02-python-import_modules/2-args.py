3e14ec4c87d9bd42d1c756ea2b1528d149152624
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-13 23:01:04 +0000

    corrections to task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__=='__main__':
 	import sys
 
 	n = len(sys.argv) - 1
-	if n==0:  
+	if n == 0:
 		print("{:d} arguments." .format(n))
 	elif n == 1:
 		print("{:d} argument:\n1: {}" .format(n, sys.argv[n]))
