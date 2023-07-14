75cb4cb45179902fb0a71b7d69e8abd14193ae9b
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-14 00:02:06 +0000

    task 4 - a program that prints all the names defined by the compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+	import hidden_4
+	hidden_4_name = dir(hidden_4)
+	for names in range(0, len(hidden_4_name)):
+		if names[0:2] != '__':
+			print("{}\n" .format(names))
