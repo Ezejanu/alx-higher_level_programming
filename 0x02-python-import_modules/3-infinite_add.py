488a6c21ad94d8881986befd6bc379254b98e6f1
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-14 03:12:48 +0000

    task 3, trying to fix pep8 errors, replacing tab spaces with space bar

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-if __name__ =="__main__":
+if __name__ == "__main__":
 
-	import sys
+    import sys
 
-	n = len(sys.argv)
-	argv_sum = 0
-	for i in range(1, n):
-		argv_sum += int(sys.argv[i])
-	print("{}" .format(argv_sum))
+    n = len(sys.argv)
+    argv_sum = 0
+    for i in range(1, n):
+        argv_sum += int(sys.argv[i])
+    print("{}" .format(argv_sum))
