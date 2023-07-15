037b276ca471f1d3eea0255f172372d866cd348a
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-15 20:26:33 +0000

    task 8 corrections - adjusting the code to check properly for when sentence is empty

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -4,4 +4,6 @@ multiple_returns = __import__('8-multiple_returns').multiple_returns
 sentence = "At school, I learnt C!"
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))
-
+sentence2 = ""
+length, first = multiple_returns(sentence2)
+print("Length: {:d} - First character: {}".format(length, first))
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    new_tuple = ()
     sentence_length = len(sentence)
-    first_char = sentence[0]
     if sentence_length == 0:
         first_char = "None"
+    else:
+        first_char = sentence[0]
     new_tuple = (sentence_length, first_char)
     return new_tuple
