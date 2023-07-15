d4074fc0c2a3b67e2f84c091e8fe886662071089
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-15 20:57:54 +0000

    task 7 - a function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-#    t_new = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
-#    for i in
-#    if len(m
-        k = 0
-#        t_new = (((tuple_a[i] + tuple_b[i]), (tuple_a[i] + tuple_b[i]))
-#        if t_new[i]:
-        t_new = tuple()
-        for a, b in zip(tuple_a[:2], tuple_b[:2])) 
-            if a and b:
-                t_new += a + b,)
-        return t_new
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 1:
+            tuple_a = (tuple_a[0], 0)
+        elif len(tuple_a) == 0:
+            tuple_a = (0, 0)
+    elif len(tuple_a) > 2:
+        tuple_a = (tuple_a[0], tuple_a[1])
+
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 1:
+            tuple_b = (tuple_b[0], 0)
+        elif len(tuple_b) == 0:
+            tuple_b = (0, 0)
+    elif len(tuple_b) > 2:
+        tuple_b = (tuple_b[0], tuple_b[1])
+    
+    tuple_new = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    return tuple_new
+
