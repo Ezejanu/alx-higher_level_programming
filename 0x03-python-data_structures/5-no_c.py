f44dfbbf183b3a53284ffd6fad68cef0ca62cf4c
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-16 20:36:12 +0000

    task 4 - correcting PEP8 errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,5 @@
 def no_c(my_string):
     if my_string:
         for i in range(len(my_string)):
-            if my_string[i] == 'C' or my_string[i] == 'c':
-                my_string.pop(my_string[i])
-        return my_string
+            if my_string[i] != 67 and my_string[i] != 99:
+                return my_string
