98249607f728c1ea34dade6fd0d8586fb779c4b2
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-17 16:42:23 +0000

    task 5- fixing a checker error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     if my_string:
         new_string = ""
         for i in my_string:
             if i != 'C' and i != 'c':
-                new_string += i
+                new_string.append(i)
         return new_string
