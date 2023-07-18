e33917df01fc6c5c61605cfa363dc1fb9f62cde8
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-18 01:11:33 +0000

    task 5 - fixing check 7 error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     if my_string:
         new_string = ""
         for i in my_string:
-            if i != 'C' and i != 'c':
+            if i not in {'c', 'C'}:
                 new_string += i
         return new_string
