de69ad4a9fd1118676476da47be23e6c526e0926
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-19 22:31:52 +0000

    task 2 corrections, checking for edge cases

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    unique_values = []
     if my_list:
         unique_values = set(my_list)
-   # print(unique_values)
         total = 0;
         for value in unique_values:
             total += value
