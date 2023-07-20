b19dde4d782d6fa596fe304f919c1500056d18d7
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-20 01:10:22 +0000

    corrections to task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     unique_values = []
     if my_list:
         unique_values = set(my_list)
-        total = 0;
+        total = 0
         for value in unique_values:
             total += value
     return total
