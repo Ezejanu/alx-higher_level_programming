2682b11aa05941b5d3e4db54ad2fbb465f7ac221
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-20 02:52:57 +0000

    corrections to task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if not a_dictionary:
-        a_dictionary = {key:value}
-    elif a_dictionary:
+    if a_dictionary:
         if key in a_dictionary:
             a_dictionary[key] = value
         elif key not in a_dictionary:
             a_dictionary[key] = value
-    return a_dictionary
+        return a_dictionary
+    return None
