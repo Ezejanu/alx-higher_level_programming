08225596d5623b37897ba862d8451640298c3e82
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-20 02:54:43 +0000

    corrections to task 7, take 1

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -6,4 +6,4 @@ def update_dictionary(a_dictionary, key, value):
         elif key not in a_dictionary:
             a_dictionary[key] = value
         return a_dictionary
-    return None
+    return {}
