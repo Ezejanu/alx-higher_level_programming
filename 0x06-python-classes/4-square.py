e98e463dbd51183f7de115535f308439f1b34d4c
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-01 19:17:32 +0000

    task 4- fixing a check

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,11 +8,6 @@ class Square:
     def __init__(self, size=0):
         """Initialising the instance variable"""
 
-        if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
         self.__size = size
 
     @property
