9bf5d1bed9caf286a14882bfe9d7c25d7ca5b8c4
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-01 21:08:18 +0000

    task 2, corrections 1

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
         """Initialising the instance variable"""
 
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
