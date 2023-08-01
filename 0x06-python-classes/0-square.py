7cedb752977b960529867e151da7c50496572540
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-01 13:55:40 +0000

    PEP 8 corrections, task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-''' This function defines a class Square '''
+""" This function defines a class Square """
+
 
 class Square:
     """ This is an empty class that does nothing """
+
     ...
     # ... can also be written as pass. They mean same thing
+
 
 square = Square()
