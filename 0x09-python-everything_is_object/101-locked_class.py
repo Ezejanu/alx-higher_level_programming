dc74007d6f700b52d4b18c619b56bb4b8e2dce62
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-08 09:56:32 -0700

    task 30, corrections, used a different implementation, slot vs setattr

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,7 +5,8 @@
 class LockedClass:
     """ A locked class with no attributes """
 
-    def __setattr__(self, name, value):
-        if name != "first_name" and not hasattr(self, 'first_name'):
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
-        super().__setattr__(name, value)
+    __slots__ = ["first_name"]
+
+
+    def __init__(self):
+        self.first_name = None
