85173b9d8c44a2782c6505924b32f473e6a575a6
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-07 19:04:03 -0700

    task 8 - Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -70,3 +70,16 @@ class Rectangle:
 
     def get_number_of_instances(cls):
         return cls.number_of_instances
+
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance (rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance (rect_1, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        area_rect_1 = rect_1.area()
+        area_rect_2 = rect_2.area()
+
+        if area_rect_1 >= area_rect_2:
+            return rect_1
+        else:
+            return rect_2
