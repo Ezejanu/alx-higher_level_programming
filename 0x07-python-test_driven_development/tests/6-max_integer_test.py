ede0539add0bd9e1a0f15c4ce2428665291ae3d4
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-11 15:31:21 -0700

    changes to unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,7 +13,7 @@ class TestMaxInteger(unittest.TestCase):
 
     def is_empty_list(self):
         """ Test for if list is empty"""
-        testList = [7]
+        testList = []
         self.assertEqual(max_integer(testList), None)
 
     def list_contains_one_element(self):
@@ -30,6 +30,7 @@ class TestMaxInteger(unittest.TestCase):
         """Testing a list with max value at the beginning"""
         testList = [102, 54, 2, 19, 45]
         self.assertEqual(max_integer(testList), 102)
+
 
     def maz_element_at_end(self):
         """Testing a list which contains multiple elements"""
