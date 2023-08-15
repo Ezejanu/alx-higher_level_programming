575bbc20640137aa4bcd90c334a7d76846e29a05
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-15 16:37:11 -0700

    Task 11.

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,8 +11,13 @@ class Student:
         self.age = age 
 
     def to_json(self, attrs=None):
-        """Retrieve a dictionary representation of Student."""
-        if (type(attrs) == list and
+        """Retrieve a dictionary represetation of Student."""
+        if (type(attrs) == list and 
                 all(type(s) == str for s in attrs)):
             return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of Student."""
+        for key, value in json.items():
+            setattr(self, key, value)
