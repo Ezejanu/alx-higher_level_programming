b4c08affd92cdaf3d6ea813fe010fa9afefcab35
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-15 15:54:15 -0700

    Task 6. wrote a function thatcreates an object from aJSON file.

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/python3
+"""Function that creates an Object from a JSON file."""
+import json
+
+
+def load_from_json_file(filename):
+    """Create an object from JSON file."""
+    with open(filename, "r") as a_file:
+        return json.load(a_file)
