a194688938ee57a5d95e30065565e633915572f1
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-14 17:18:09 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is an empty class"""
+
+
+class BaseGeometry:
+    """This is an empty class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
