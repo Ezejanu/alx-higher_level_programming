ad37e139861b7ee1ba699c518e905505838ea35e
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-21 17:22:29 -0700

    created test file for task 2

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_Rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is  a unittest for class rectangle"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.Testcase):
+    def test_rectangle(self)
