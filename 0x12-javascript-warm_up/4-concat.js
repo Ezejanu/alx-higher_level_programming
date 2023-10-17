c494113cf168b2c99d2fee1d99a4b04e193ab2ae
Ezejanu <Ezejanu_nno@ymail.com>
2023-10-16 17:52:06 -0700

    fixed semistandard errors

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-let format = " is ";
+const format = ' is ';
 const argv = process.argv;
 
 if (argv[2] === undefined) {
-  console.log("undefined" + format + "undefined");
+  console.log('undefined' + format + 'undefined');
 } else if (argv.length === 3) {
-  console.log(argv[2] + format + "undefined");
+  console.log(argv[2] + format + 'undefined');
 } else {
   console.log(argv[2] + format + argv[3]);
 }
