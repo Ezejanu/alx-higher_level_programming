0e9d847b93a09520188253089d42029fb34f6961
Ezejanu <Ezejanu_nno@ymail.com>
2023-10-22 21:54:05 -0700

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+"""
+    Connects to a MySQL database using provided credentials and retrieves
+    and prints all rows from the 'states' table in ascending order of 'id'.
+
+    Usage: python script_name.py <username> <password> <database_name>
+
+    Args:
+        sys.argv[1]: MySQL username
+        sys.argv[2]: MySQL password
+        sys.argv[3]: MySQL database name
+        sys.argv[4]: state name searched
+
+    Returns:
+        None
+"""
+
+if __name__ == "__main__":
+
+    username, password, database, state_searched = sys.argv[1:5]
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cursor = connection.cursor()
+    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+    # Define params as a tuple)
+    params = (state_searched,)
+
+    cursor.execute(sql_query, params)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    connection.close()
