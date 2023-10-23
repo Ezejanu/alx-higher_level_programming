f054795281eaa0db53c7e1aa1ac7e5ed073fa3ea
Ezejanu <Ezejanu_nno@ymail.com>
2023-10-22 21:26:56 -0700

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+"""
+    Connects to a MySQL database using provided credentials and retrieves
+    and prints all rows from the 'states' table in ascending order of 'id'.
+
+    Usage: python script_name.py <username> <password> <database_name>
+
+    Args:
+        sys.argv[1]: MySQL username
+        sys.argv[2]: MySQL password
+        sys.argv[3]: MySQL database name
+
+    Returns:
+        None
+"""
+
+if __name__ == "__main__":
+
+    username, password, database = sys.argv[1:4]
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    connection.close()
