9d13cca8b2d24211dfbbcc1889bb4a605309d6a3
Ezejanu <Ezejanu_nno@ymail.com>
2023-11-02 14:22:53 -0700

    fixing task 1, changed the implementation used

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a GET request to a URL, and displays body of the response
-curl -s -o /dev/null -w "%{http_code}" -i "$1" | awk '/^200$/ {p=1; next} p'
+curl -sL "$1"
