63f713e2f77211e60712bd123fa1fa2b37166b82
Ezejanu <Ezejanu_nno@ymail.com>
2023-11-02 10:43:34 -0700

    correction to task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a GET request to a URL, and displays body of the response
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && echo "$(curl -s "$1")"
+[ $(curl -s -o /dev/null -w "%{http_code}" "$1") -eq 200 ] && curl -s "$1"
