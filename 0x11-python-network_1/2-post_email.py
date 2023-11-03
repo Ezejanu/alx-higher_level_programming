3097af454a121deff23b7f9877a870f3e27caa5b
Ezejanu <Ezejanu_nno@ymail.com>
2023-11-03 16:15:53 -0700

    fixed pycodestyke error task 6, included decoding in task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,9 +14,10 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = urllib.parse.urlencode({'email': email})
     data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(req) as response:
         content = response.read().decode('utf-8')
+        print(content)
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,7 +9,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    
+
     url = sys.argv[1]
     email = sys.argv[2]
 
