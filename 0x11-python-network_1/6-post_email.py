7a3e8db235fa2c351228a86db58ff0eba2e6e7a9
Ezejanu <Ezejanu_nno@ymail.com>
2023-11-03 16:12:49 -0700

    task 6 - included if __name__ == "__main__":

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,10 +8,12 @@ and finally displays the body of the response.
 import requests
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = {'email': email}
+    data = {'email': email}
 
-response = requests.post(url, data=data)
-print(response.text)
+    response = requests.post(url, data=data)
+    print(response.text)
