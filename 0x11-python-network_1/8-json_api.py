daed7b7d0504b87bb4762d6138f5ded2e266b8cd
Ezejanu <Ezejanu_nno@ymail.com>
2023-11-03 17:46:26 -0700

    trying an alternative implementation

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,23 +9,27 @@ import sys
 
 if __name__ == "__main__":
 
-    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    #    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        if all(65 <= ord(char) <= 90 or 97 <= ord(char) <= 122
+                for char in arg):
+            q = arg
+        else:
+            q = ""
+    else:
+        q = ""
+
     url = "http://0.0.0.0:5000/search_user"
     data = {"q": q}
 
     response = requests.post(url, data=data)
 
-    print("Response Status Code:", response.status_code)
-    print("Response Content:", response.text)
-
-    if response.status_code == 200 and response.text:
-        try:
-            data = response.json()
-            if data:
-                print("[{}] {}".format(data.get("id"), data.get("name")))
-            else:
-                print("No result")
-        except ValueError:
-            print("Not a valid JSON")
-    else:
-        print("No result")
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
