4d5178ba260f750e0efeb26d9efcedd48a37274f
Ezejanu <Ezejanu_nno@ymail.com>
2023-11-28 03:24:04 -0800

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// A script that reads and prints the content of a file
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+
+fs.readFile(filepath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(data);
+}
+);
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,2 @@
+This is a project on JavaScript - Web scraping
+The project consists of six (6) mandatory tasks and two (2) advanced/optional tasks
