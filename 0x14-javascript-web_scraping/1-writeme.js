e16a5a0883310636da5a1806a36380831fb49319
Ezejanu <Ezejanu_nno@ymail.com>
2023-11-28 04:04:30 -0800

    task 1, also included comments in task 0

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,16 +6,13 @@ const fs = require('fs');
 
 // Get the file path from the command line argument
 const filepath = process.argv[2];
+const filecontent = process.argv[3];
 
-// Read the file in utf-8
-fs.readFile(filepath, 'utf-8', (err, data) => {
+// Write the file in utf-8
+fs.writeFile(filepath, filecontent, 'utf-8', (err) => {
   // Handle errors, if any
   if (err) {
     console.error(err);
-    return;
   }
-
-  // Print the content of the file
-  console.log(data);
 }
 );
